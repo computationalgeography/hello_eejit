7ffb71392b7c7bf047010fcd9acd58de5afa1b8d
Kor de Jong <k.dejong1@uu.nl>
2021-03-02 08:07:30 +0100

    Refactor, comment

--- say_hello_to_eejit.sh
+++ say_hello_to_eejit.sh
@@ -2,37 +2,38 @@
 set -e
 
 
+# Environment ------------------------------------------------------------------
 # Load software modules
 # TODO
 # module load ...
-
 
 # These should be set:
 echo "BOOST_ROOT     : $BOOST_ROOT"
 echo "HDF5_ROOT      : $HDF5_ROOT"
 echo "GDAL_ROOT      : $GDAL_ROOT"
 
-# This one should not point to boost, hdf5, ...:
+# This one should not point to directories containing shared libraries
+# for Boost, HDF5, .... There is no need for LD_LIBRARY_PATH.
 echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 
 
+# Pathnames --------------------------------------------------------------------
 tmp_prefix=$(mktemp --directory --tmpdir hello_eejit-XXXX)
 build_prefix="$tmp_prefix/build"
 install_prefix="$tmp_prefix/install"
 echo "All files will be stored in $tmp_prefix"
 
-# Generate build scripts
+
+# Configure, build, install ----------------------------------------------------
 cmake -S . -B $build_prefix -DCMAKE_INSTALL_PREFIX:PATH=$install_prefix
-
-# Build software
 cmake --build $build_prefix
-
-# Install software
 cmake --install $build_prefix
 
-# Show dependencies of installed target on shared libraries
-# All of them should be resolved
+
+# Tests ------------------------------------------------------------------------
+# Show dependencies of installed target on shared libraries (Boost, HDF5, ...).
+# All of them should be resolved. There is no need for LD_LIBRARY_PATH.
 ldd $install_prefix/bin/say_hello_to_eejit
 
-# Make sure target works, without having to expand LD_LIBRARY_PATH
+# Make sure target works. There is no need for LD_LIBRARY_PATH.
 $install_prefix/bin/say_hello_to_eejit
